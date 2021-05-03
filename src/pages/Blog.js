5490dfc8fa982240768fcc4c245fab48d9b24fc3
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2021-05-03 10:46:00 +0700

    post data with fake api

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -7,15 +7,29 @@ import axios from 'axios'
 class Blog extends Component {
 
     state = {
-        post: []
+        post: [],
+        formPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        }
     }
     
     getAllPost = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then((result) => {
             this.setState({
                 post: result.data
             })
+        })
+    }
+
+    postData = () => {
+        axios.post('http://localhost:3004/posts', this.state.formPost).then((res) => {
+            console.log(res);
+        }, (err) => {
+            console.log('error', err);
         })
     }
 
@@ -25,6 +39,21 @@ class Blog extends Component {
         })
     }
 
+    handleFormChange = (event) => {
+        let formPostNew = {...this.state.formPost};
+        let timestamp =  new Date().getTime();
+        formPostNew['id'] = timestamp;
+        formPostNew[event.target.name] = event.target.value
+        this.setState({
+            formPost: formPostNew
+        })
+        
+    }
+
+    handleSubmit = () => {
+        this.postData();
+    }
+
     componentDidMount() {
         this.getAllPost();
     }
@@ -32,8 +61,25 @@ class Blog extends Component {
     render() {
         return (
             <Fragment>
-                <div className="text-center mt-3">
-                    <h2>Welcome to my Blog</h2>
+                <div className="container">
+                    <div className="text-center mt-3">
+                        <h2>Welcome to my Blog</h2>
+                    </div>
+                    <div className="row mb-3 px-3">
+                        <div className="card col-12 shadow">
+                            <div className="py-3 px-3">
+                                <div className="form-group">
+                                    <label className="mr-3">TITLE</label>
+                                    <input type="text" className="form-control" id="title" name="title" placeholder="Enter Title Here" onChange={this.handleFormChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label className="mr-3">Desc</label>
+                                    <textarea type="text" className="form-control" id="body" name="body" placeholder="Enter Content Here" onChange={this.handleFormChange} />
+                                </div>
+                                <button className="btn btn-outline-primary float-right" onClick={this.handleSubmit}>Save</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 {
                     this.state.post.map(post => {
